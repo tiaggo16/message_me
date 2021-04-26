0a0b5141084fa5708214f0efd5e6c59220cbaab5
tabosa <tiaggo95@hotmail.com>
2021-04-26 17:50:27 -0300

    add actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@ User.create(username: "tiago", password: "password1")
 User.create(username: "alice", password: "password2")
 User.create(username: "rafinha", password: "password3")
 User.create(username: "jeca", password: "password4")
+
+Message.create(body: "Hello everyone", user_id: 1)
+Message.create(body: "Hey new guy", user_id: 2)
+Message.create(body: "I'm chilling like ice", user_id: 3)
