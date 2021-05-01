fdade382a9f1a46128610cd8d3c3c503d295b96e
tabosa <tiaggo95@hotmail.com>
2021-05-01 00:31:51 -0300

    add working messages at log in attempt

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,8 @@ require("semantic-ui-sass")
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
 
